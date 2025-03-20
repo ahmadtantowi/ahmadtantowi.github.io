e768aafc171fe1a1e934a740a25d812186e57ed8
Ahmad Tantowi <ahmad.tantowi@efishery.com>
2025-03-20 21:22:21 +0700

    Add google analytics id

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -168,7 +168,7 @@ const CONFIG = {
     limit: 2, // How many articles to display. Max is 10.
   },
   googleAnalytics: {
-    id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
+    id: 'G-2BVWPD3BHJ', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
   },
   // Track visitor interaction and behavior. https://www.hotjar.com
   hotjar: {
